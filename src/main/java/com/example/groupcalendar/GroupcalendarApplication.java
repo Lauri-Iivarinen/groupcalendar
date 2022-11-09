05446cc9a03ff7ee12a6bda2c60066a2aac34bc4
Lauri <lauri.iivarinen@gmail.com>
2022-11-09 11:58:57 +0200

    spring beans

--- src/main/java/com/example/groupcalendar/GroupcalendarApplication.java
+++ src/main/java/com/example/groupcalendar/GroupcalendarApplication.java
@@ -27,7 +27,7 @@ public class GroupcalendarApplication {
 		SpringApplication.run(GroupcalendarApplication.class, args);
 	}
 
-	/*
+	
 	//kommentoitu poist kun postgre käytössä
 	@Bean
 	public CommandLineRunner launchTestBook (UserRepository userRepo, GroupRepository groupRepo, EventRepository eventRepo) {
